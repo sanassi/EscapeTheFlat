9af926fc17e860390a5d6a32a4ee775119d55950
sanassi <sanassicisse@outlook.fr>
2020-10-14 19:38:02 +0200

    walls + camera fixed

---
+++ Test_Movement_3D_Iso/Assets/CharController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class CharController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed = 10000f;
+
+    private Vector3 _forward, _right;
+    void Start()
+    {
+        _forward = Camera.main.transform.forward;
+        _forward.y = 0;
+        _forward = Vector3.Normalize(_forward);
+        _right = Quaternion.Euler(new Vector3(0, 90, 0)) * _forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 rightMovement = Time.deltaTime * moveSpeed *  Input.GetAxis("Horizontal") * _right;
+        Vector3 upMovement = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical") * _forward;
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+}
